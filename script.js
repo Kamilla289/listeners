12d12be90d68f06be287b16d2c600b5b6620f6e0
Kamilla289 <kami.rivay56@gmail.com>
2024-11-10 17:02:55 +0500

    event object

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+'use strict';
 
+const button = document.querySelector('#btn');
+const inputText = document.querySelector('#text');
+const square = document.querySelector('#square');
+const eButton = document.querySelector('#e_btn');
+const inputRange = document.querySelector('#range');
+const circle = document.querySelector('#circle');
 
+button.addEventListener('click', function() {
+const textValue = inputText.value;
+square.style.backgroundColor = textValue;
+});
 
+eButton.style.display = 'none';
 
-
+inputRange.addEventListener('input', function() {
+const rangeValue = inputRange.value;
+circle.style.width = rangeValue + '%';
+circle.style.height = rangeValue + '%';
+});
